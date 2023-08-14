0d97c396d6c2b2301159b8da5adf40dfbf297815
Anubha Tomar <anubhav.singh@redbus.com>
2023-08-14 10:01:29 +0000

    updated layout with flex and animated header

--- src/Page/Home/Y2s.js
+++ src/Page/Home/Y2s.js
@@ -1,10 +1,57 @@
+
 import React, { useState } from 'react';
 import { Button, Form, Input, Radio,Layout,message} from 'antd';
+// import './test.css';
 import { DownloadAndPlay } from '../../Utils/Player';
 import axios from 'axios'
 import { Y2sList } from '../../Components/Y2S/Y2slist';
 
+// import React from "react";
+import styled from "styled-components";
+import { keyframes } from "styled-components";
 
+/**
+ * Example Text Gradient Animation
+ */
+// export default function TextGradientComponent() {
+//   return (
+//     <>
+//       <AnimatedGradientText>Hi, I'm Alexander</AnimatedGradientText>
+//     </>
+//   );
+// }
+
+const gradient = keyframes`
+{
+0% {
+  background-position: 0 50%;
+}
+50% {
+  background-position: 100% 50%;
+}
+
+100% {
+  background-position: 0 50%;
+}}
+`;
+const AnimatedGradientText = styled.h1`
+  animation: ${gradient} 5s ease-in-out infinite;
+  background: linear-gradient(to right, #ee9ca7, #ffdde1, #2193b0, #6dd5ed);
+  background-size: 300%;
+  background-clip: text;
+
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
+const AnimatedGradientBg = styled.h1`
+  animation: ${gradient} 5s ease-in-out infinite;
+  background: linear-gradient(to right, #aea, #fe6, #784da9, #1bf893);
+  
+  background-size: 500%;
+  background-clip: text;
+  // -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
 const { Header, Content, Footer, Sider } = Layout;
 
 export function ChangeDisableStop() {
@@ -114,33 +161,171 @@ export function Y2s() {
         setpayload(url)
     }
 
+    const stl_header =  {
+      color: "white",
+      backgroundColor: "grey",
+      height:'10vh',
+      // padding: "10px",
+      // display:'flex',
+      // justifyContent:'center',
+      fontSize:'30pt',
+      fontFamily: "Helvetica",
+      overflow:"hidden",
+      textAlign:'center'
+      // textAlign:'justify'
+
+    };
+    const aspan={
+      backgroundColor:'red',
+      width:'100%',
+      height:"1em",
+      display:'inline-block'
+    };
+    
+    const stl_toolbar =  {
+      // wrap:'nowrap',
+      // whiteSpace: 'nowrap',
+      flex:1,
+      padding: ".1rem",
+      // alignContent:'center',
+      color: "white",
+      display: "flex",
+      height:'10vh',
+      // width:'80vw',
+      alignItems:'center',
+      flexDirection : 'column',
+      justifyContent :'flex-start',
+      // backgroundColor: "white",
+      // alignItems: 'stretch'
+      
+    };
+    const stl_form =  {
+      // color: "white",
+      // margin:'auto',
+      // display: "flex",
+      // justifyContent :'space-around',
+      // alignItems:'center',
+      backgroundColor: "pink",
+      
+    };
+
+    const stl_urlbar =  {
+      // color: "white",
+      // margin:'.2rem .5rem',
+      // alignItems:'center',
+      // flexGrow:1,
+      margin: '1rem 0rem',
+      padding: '2rem',
+      // width: '80vw',
+      // flexWrap: 'wrap',
+      border :'2px dotted #bbb',
+      borderRadius:'2.5rem',
+      textAlign: 'center',
+      // justifyContent: 'center',
+      // alignItems: 'center',
+      // display: "flex",
+      // flexDirection : 'column',
+      // flexDirection:'center',
+      // justifyContent:'center',
+      // backgroundColor: "#7490AB",
+      background: 'linear-gradient(to bottom, #eee 0%, #77ab9f 100%)'
+      // background: 'linear-gradient(to bottom, #ffffff 0%, #BBB 100%)'
+  
+    };
+    const stl_btns =  {
+      // color: "white",
+      // margin: '2rem 0rem',
+      padding:'.2rem .5rem',
+      margin:'1rem 0rem',
+      display: "flex",
+      // backgroundColor: 'grey',
+      
+
+      flexDirection : 'row',
+      // flexDirection:'center',
+      justifyContent:'',
+      // backgroundColor: "black",
+  
+    };
+
+    const stl_btn =  {
+      // color: "white",
+      margin:'.2rem .5rem',
+      
+      // display: "flex",
+      // flexDirection : 'column',
+      // flexDirection:'center',
+      // justifyContent:'center',
+      backgroundColor: "#11ab9f",
+  
+    };
+
+    const stl_list =  {
+      color: "white",
+      display: "flex",
+      height:'100vh  ',
+      // flex:1,
+
+      // flexDirection:'center',
+      justifyContent:'center',
+      backgroundColor: "black",
+  
+    };
+
+    const some_css = { "backgroundColor":'red',"display":'flex'};
+
     return(
-        
-         <Layout style={{background : "#7490AB" ,padding : "100px"}}>
-      <Layout className="layout">
-        <Content style={{padding : 30 }}>
-        <Form.Item >
-        <Input disabled={disabled} onChange={(e)=>captureUrl(e.target.value)} placeholder="Please Enter YOutube Link" />
-        <Button type="primary" onClick={callDownloadAndPlay}>Play-save</Button>
-        {/* <Button disabled={disbaleStop} type="primary" onClick={kill}>Stop</Button> */}
-        <Button  type="primary" onClick={kill}>Stop</Button> 
-        <Button  type="primary" onClick={()=>UtilHandle("UP")}>Vol Up</Button> 
-        <Button  type="primary" onClick={()=>UtilHandle("DOWN")}>Vol Down</Button> 
-        <Button  type="primary" onClick={()=>UtilHandle("MUTE")}>Mute</Button> 
+        // <div className='some_css'>
+        //  <AnimatedGradientBg style={{"display":"block","overflow":'hidden',"margin":'0 0',"width":'100%'}}>BG</AnimatedGradientBg>
+        <Layout style={{background : "#acaa99" , display:'flex',flexWrap: 'wrap',flexDirection:'column'}}>
+           <Content>
+            <AnimatedGradientBg style={{"display":"block","overflow":'hidden',"margin":'0 0',"width":'100%',"height":'20vh'}}>  <>
+              <AnimatedGradientText style={{"position":"absolute", "fontSize":'5.5rem',
+      "fontFamily": "Helvetica",
+      "overflow":"hidden",
+      "textAlign":'center',}}>CURRENTLY PLAYING THIS REALLY NICE SONG</AnimatedGradientText>
+              </></AnimatedGradientBg>
+          {/* <Layout style={{background : "#7490AB" ,padding : "100px",width: "100vw",height:"100vw"}}> */}
+            {/* <Content style={{backgroundColor:'salmon'}}> */}
+              {/* <Header className="header" style={stl_header}> */}
+                
+               </Content>
+            
+              {/* <span style={aspan}> */}
+              {/* </span> */}
+              {/* <p className='animated-gradient'> CURRENTLY PLAYING THIS </p> */}
+              {/* <h1 class="closure">");</h1> */}
+             
+              {/* </Header> */}
+              <Content className="toolbar" style={stl_toolbar}>
+                
+              
+                <Form.Item  >
+                <Input disabled={disabled}  style={stl_urlbar} onChange={(e)=>captureUrl(e.target.value)} placeholder="Please Enter Youtube Link" />
+               
+                <div className="btns" style={stl_btns} >
+                  <Button className='btn' style={stl_btn} type="primary" onClick={callDownloadAndPlay}>Play-save</Button>
+                  {/* <Button disabled={disbaleStop} type="primary" onClick={kill}>Stop</Button> */}
+                  <Button   className='btn' style={stl_btn} type="primary" onClick={kill}>Stop</Button> 
+                  <Button   className='btn' style={stl_btn} type="primary" onClick={()=>UtilHandle("UP")}>Vol Up</Button> 
+                  <Button   className='btn' style={stl_btn} type="primary" onClick={()=>UtilHandle("DOWN")}>Vol Down</Button> 
+                  <Button  className='btn' style={stl_btn} type="primary" onClick={()=>UtilHandle("MUTE")}>Mute</Button> 
+                </div>
 
+                </Form.Item>
+            </Content>
+            <Content className="list" style={stl_list}>
+                <Y2sList/>
+            </Content>
 
-        <Y2sList/>
-       
-        </Form.Item>
-        </Content>
-      </Layout>
-      <Footer
-          className="layout" 
-        >
-          @XPNS
-        </Footer>
+        {/* </Layout> */}
+        {/* <Footer
+            className="layout" 
+          >
+            @XPNS
+          </Footer> */}
     </Layout>
-        
+   
         
     );
 }
